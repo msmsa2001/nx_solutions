2713cb7558306df4913e20f449c8584b7b5197e9
Rohan. <rohandas2468@gmail.com>
2025-05-07 22:23:08 +0000

    Update Aevis script source URL to use the correct domain

--- assets/js/aevis.js
+++ assets/js/aevis.js
@@ -5,4 +5,4 @@ window.AevisSettings = {
   user_hash: "9fa1a7d7061945500d1c2bc74219d4da7bce32b71d9af0674599a81b2b14a3be",
 };
 
-!function(){var w=window,t=w.Aevis&&w.Aevis.init;if("function"==typeof t)t();else{var n=document,f=function(){var e=n.createElement("script");e.type="text/javascript",e.defer=!0,e.src="https://codenet.aevis.io/copilot/?app_id="+APP_ID,e.onload=function(){"undefined"!=typeof Aevis&&Aevis.init()};var t=n.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t)};"complete"===n.readyState?f():w.attachEvent?w.attachEvent("onload",f):w.addEventListener("load",f,!1)}}();
+!function(){var w=window,t=w.Aevis&&w.Aevis.init;if("function"==typeof t)t();else{var n=document,f=function(){var e=n.createElement("script");e.type="text/javascript",e.defer=!0,e.src="https://aevis-codenet.cruv.dev/copilot/?app_id="+APP_ID,e.onload=function(){"undefined"!=typeof Aevis&&Aevis.init()};var t=n.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t)};"complete"===n.readyState?f():w.attachEvent?w.attachEvent("onload",f):w.addEventListener("load",f,!1)}}();
